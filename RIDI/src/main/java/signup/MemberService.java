f65de57b40cb1aaf14e825dc9daa5fdff2249a7f
DESKTOP-68UF4CQ\USER <ekdh0858@naver.com>
2022-02-21 12:03:32 +0900

    no message

--- RIDI/src/main/java/signup/MemberService.java
+++ RIDI/src/main/java/signup/MemberService.java
@@ -46,25 +46,26 @@ public class MemberService {
 		// 비밀번호와 비밀번호 확인의 값은 같아야함.
 		if(!Pw.equals(Pw2)) { 
 			statusCode = 401; return statusCode;}
-		if(!Email.contains("@naver.com") || Email.contains("@google.com")) { statusCode = 401; return statusCode;}
+//		if(!Email.contains("@naver.com") || Email.contains("@google.com")) { statusCode = 401; return statusCode;}
 		
 		//회원가입 규칙 
 		// 1. 아이디 : 5~20자의 영문,숫자로 이루어짐
 		int ValSort=0;
 		boolean IdValidator = Pattern.matches("^\\w{5,20}$", Id);
-		System.out.println(Pattern.matches("^[a-zA-Z]+$" ,Id));
-		if(Pattern.matches("^[a-zA-Z]+$" ,Id)) ValSort++;
-		System.out.println(ValSort);
-		if(Pattern.matches("^[0-9]+$" ,Id)) ValSort++;
-		System.out.println(ValSort);
-		if(Pattern.matches("^[!@#$%^&*()?_~]+$" ,Id)) ValSort++;
-		System.out.println(ValSort);
+		
 		if(ValSort<2) IdValidator=false;
 		if(IdValidator==false) {statusCode=401;System.out.println("Id 검증 실패 : "+Id);return statusCode;}
 		
 		// 2. 비밀번호 : 8자 이상, 영문/숫자/특수문자 중 2가지 이상 입력
-		boolean Pwvalidator = Pattern.matches(Sex, Pw);
-		if(Pwvalidator==false) {statusCode=401;System.out.println("Pw 검증 실패 : "+Pw);return statusCode;}
+		boolean PwValidator = Pattern.matches(Sex, Pw);
+		if(Pattern.matches("[a-zA-Z]+" ,Id)) ValSort++;
+		System.out.println(ValSort);
+		if(Pattern.matches("[0-9]+" ,Id)) ValSort++;
+		System.out.println(ValSort);
+		if(Pattern.matches("[!@#$%^&*()?_~<>{}[\\].,;:\"']+" ,Id)) ValSort++;
+		System.out.println(ValSort);
+		if(ValSort<2) PwValidator=false;
+		if(PwValidator==false) {statusCode=401;System.out.println("Pw 검증 실패 : "+Pw);return statusCode;}
 		
 		// 3. 이메일 주소 : @가 있어야함. @다음에 .이 있어야함
 		boolean Emailvalidator = Pattern.matches(Sex, Email);
