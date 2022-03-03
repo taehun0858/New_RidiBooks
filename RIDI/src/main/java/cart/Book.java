3e887a4dda18f4376a457563619278367ea2145b
DESKTOP-KF4NQR7\Taehun <ekeh0858@naver.com>
2022-03-03 23:07:55 +0900

    커밋

--- RIDI/src/main/java/cart/Book.java
+++ RIDI/src/main/java/cart/Book.java
@@ -6,36 +6,54 @@ public class Book {
 	String Publisher;
 	int bookNum;
 	String imageUrl;
+	String author;
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
 	public String getImageUrl() {
 		return imageUrl;
 	}
+
 	public void setImageUrl(String imageUrl) {
 		this.imageUrl = imageUrl;
 	}
+
 	public String getTitle() {
 		return Title;
 	}
+
 	public void setTitle(String title) {
 		Title = title;
 	}
+
 	public int getPrice() {
 		return Price;
 	}
+
 	public int getBookNum() {
 		return bookNum;
 	}
+
 	public void setBookNum(int bookNum) {
 		this.bookNum = bookNum;
 	}
+
 	public void setPrice(int price) {
 		Price = price;
 	}
+
 	public String getPublisher() {
 		return Publisher;
 	}
+
 	public void setPublisher(String publisher) {
 		Publisher = publisher;
 	}
-	
-	
+
 }
--- RIDI/src/main/java/cart/CartController.java
+++ RIDI/src/main/java/cart/CartController.java
@@ -5,6 +5,7 @@ import java.io.PrintWriter;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -17,12 +18,12 @@ import javax.servlet.http.HttpSession;
 public class CartController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// 카트 페이지로 이동했을때 로그인한 아이디가 카트에 등록해놓은 책들 목록을 전달해줘야함.
 		request.setCharacterEncoding("UTF-8");
 		response.setCharacterEncoding("UTF-8");
 		HttpSession session = request.getSession();
-		if(session.getAttribute("isLogin")!=null || Integer.parseInt((String) session.getAttribute("isLogin"))!=0) {
+		if(session.getAttribute("isLogin")!=null) { // || Integer.parseInt((String) session.getAttribute("isLogin"))!=0) {
 			// 로그인을 하고 카트페이지에 왔을때
 			Integer loginIdNum = (Integer) session.getAttribute("isLogin");
 			//loginIdNum은 세션의 isLogin에 저장되어있는 로그인한 회원의 고유번호를 가져옴
@@ -34,6 +35,7 @@ public class CartController extends HttpServlet {
 			String[] data = new String[books.size()];
 			for (int i = 0; i <= books.size() - 1; i++) {
 				data[i] = " {\"title\": \"" + books.get(i).getTitle() + "\", \"publisher\":\""+ books.get(i).getPublisher()
+						+"\", \"bookNum\":\""+ books.get(i).getBookNum()+"\", \"imageurl\":\""+ books.get(i).getAuthor()
 						+"\", \"imageurl\":\""+ books.get(i).getImageUrl()+  "\"}";
 			}
 			String result = Arrays.toString(data);
@@ -43,6 +45,8 @@ public class CartController extends HttpServlet {
 //			request.setAttribute("CartBooks", books);
 			response.setStatus(HttpServletResponse.SC_OK);
 		}else {
+			RequestDispatcher rd = request.getRequestDispatcher("/login.html");
+			rd.forward(request, response);
 			response.setStatus(HttpServletResponse.SC_BAD_GATEWAY);
 		}
 		
